75624cdab2de6ee6990bb574d0f97a1aa101b0f1
Aloyzas <aloyzas@scoding.com>
2020-05-16 15:36:06 +0300

    Added created_at field to coffee order

--- src/Entity/CoffeeOrder.php
+++ src/Entity/CoffeeOrder.php
@@ -4,12 +4,15 @@ namespace App\Entity;
 
 use App\Repository\CoffeeOrderRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity(repositoryClass=CoffeeOrderRepository::class)
  */
 class CoffeeOrder
 {
+    use TimestampableEntity;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
