cd01f8fa67a6d9feba3dde2afa6e44ea0e967f6c
Aloyzas <aloyzas@scoding.com>
2020-05-16 11:26:47 +0300

    Added map for delivery location select in coffee order form

---
+++ public/js/maps.js
@@ -0,0 +1,40 @@
+var map;
+var marker = false;
+        
+function initMap() {
+    var centerOfMap = new google.maps.LatLng(54.894983711212056, 23.903343272805177);
+ 
+    var options = {
+      center: centerOfMap,
+      zoom: 12
+    };
+ 
+    map = new google.maps.Map(document.getElementById('map'), options);
+
+    google.maps.event.addListener(map, 'click', function(event) {                
+        var clickedLocation = event.latLng;
+
+        if(marker === false) {
+            marker = new google.maps.Marker({
+                position: clickedLocation,
+                map: map,
+                draggable: true //make it draggable
+            });
+
+            google.maps.event.addListener(marker, 'dragend', function(event) {
+                markerLocation();
+            });
+        } else {
+            marker.setPosition(clickedLocation);
+        }
+
+        markerLocation();
+    });
+}
+    
+function markerLocation() {
+    var currentLocation = marker.getPosition();
+    document.getElementById('location').value = currentLocation.lat()+', '+currentLocation.lng();
+}
+    
+google.maps.event.addDomListener(window, 'load', initMap);
